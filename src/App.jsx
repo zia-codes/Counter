d1cb5e57f6dafae1ef3622c21aaa31e5a6007d5a
zia-codes <zia-codes@users.noreply.github.com>
2026-01-09 15:44:49 +0500

    add children prop

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 export default function App() {
   return (
     <div>
+      <Steps />
+      <Steps />
       <Counter />
-      <Steps />
-      <Steps />
     </div>
   );
 }
@@ -42,23 +42,25 @@ function Steps() {
             {/* {test.name} */}
           </p>
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button bgColor='#7950f2' textColor='#fff' onClick={handlePrevious}><span>👈</span>Previous</Button>
+            <Button bgColor='#7950f2' textColor='#fff' onClick={handleNext}>Next<span>👉</span></Button>
+           
           </div>
         </div>
       )}
     </div>
   );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+              style={{ backgroundColor: bgColor, color: textColor }}
+              onClick={onClick}
+            >
+              {children}
+            </button>
+  ) 
 }
 
 function Counter() {
